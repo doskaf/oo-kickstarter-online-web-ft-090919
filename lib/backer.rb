61efb749297e76dc740ca5447be26a0550261662
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 01:21:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,11 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
+<<<<<<< HEAD
     project.backers << self
+=======
+    Project.backers << self
+>>>>>>> 89e7628615f6ad28c69e1c7717796ed82e00ed50
   end
   
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,11 @@
 class Project
   
+<<<<<<< HEAD
   attr_reader :title, :backers
+=======
+  attr_reader :title
+  attr_accessor :backers
+>>>>>>> 89e7628615f6ad28c69e1c7717796ed82e00ed50
   
   def initialize(title)
     @title = title
@@ -9,7 +14,10 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+<<<<<<< HEAD
     backer.backed_projects << self
+=======
+>>>>>>> 89e7628615f6ad28c69e1c7717796ed82e00ed50
   end
   
 end
